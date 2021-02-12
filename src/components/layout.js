2bf043f02e329b3e73455511c0f8380d20a34136
sakakinox <sakakinox@gmail.com>
2021-02-12 14:09:55 +0900

    add navigationbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import NavicationBar from "../components/nav"
+
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
 
-  if (isRootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <header>
+       <NavicationBar title={title} />  
+      </header>
     )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
