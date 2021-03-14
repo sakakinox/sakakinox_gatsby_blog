9c9ee086e42ad2e6f669390d6e3d726832a0167f
sakakinox <sakakinox@gmail.com>
2021-03-14 19:18:19 +0900

    change tr_id

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "process.env.GOOGLE_ANALYTICS_ID",
+        trackingId: `${process.env.GOOGLE_ANALYTICS_ID}`,
         head: true,
       },
     },
