b56b1859cc652fc074895c9744e506fc1f35e6d5
sakakinox <sakakinox@gmail.com>
2021-03-14 16:45:42 +0900

    del unload css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-//import "./src/normalize.css"
 // custom CSS styles
-//import "./src/style.css"
 import "./src/styles/style.js"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
