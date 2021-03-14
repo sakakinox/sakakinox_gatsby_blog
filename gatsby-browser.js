fec8826b60a99dd9cceea830b591a50d1b0fa4a5
sakakinox <sakakinox@gmail.com>
2021-03-14 17:10:03 +0900

    fix warning

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,5 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 // custom CSS styles
-import "./src/styles/style.js"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Bio from './bio';
 
 const useStyles = makeStyles((theme) => ({
