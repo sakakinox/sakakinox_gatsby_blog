d0e1ca68c661b080fdab73454ebcf4b074cd9128
sakakinox <sakakinox@gmail.com>
2021-12-11 13:54:18 +0000

    update gatsby v4

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,55 +7,51 @@
 
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
-import Image from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image";
 import TwitterIcon from "@material-ui/icons/Twitter"
 import GitHubIcon from "@material-ui/icons/GitHub"
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled"
 import { Grid, IconButton } from "@material-ui/core"
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/Fax2lqiP_400x400.png/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50, quality: 95) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+  const data = useStaticQuery(graphql`query BioQuery {
+  avatar: file(absolutePath: {regex: "/Fax2lqiP_400x400.png/"}) {
+    childImageSharp {
+      gatsbyImageData(width: 50, height: 50, quality: 95, layout: FIXED)
+    }
+  }
+  site {
+    siteMetadata {
+      author {
+        name
+        summary
       }
-      site {
-        siteMetadata {
-          author {
-            name
-            summary
-          }
-          social {
-            twitter
-            github
-            lastfm
-          }
-        }
+      social {
+        twitter
+        github
+        lastfm
       }
     }
-  `)
+  }
+}
+`)
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
 
-  const avatar = data?.avatar?.childImageSharp?.fixed
+  const avatar = data?.avatar?.childImageSharp?.gatsbyImageData
 
   return (
     <Grid container justify="center" alignItems="center" direction="column">
       <Grid item xs={12}>
         {avatar && (
-          <Image
-            fixed={avatar}
+          <GatsbyImage
+            image={avatar}
             alt={author?.name || ``}
             imgStyle={{
               borderRadius: `50%`,
-            }}
-          />
+            }} />
         )}
       </Grid>
       <Grid item>
@@ -83,7 +79,7 @@ const Bio = () => {
         )}
       </Grid>
     </Grid>
-  )
+  );
 }
 
 export default Bio
--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from "@material-ui/core/styles"
-import { red } from "@material-ui/core/colors";
+import { createTheme } from "@material-ui/core/styles"
+//import { red } from "@material-ui/core/colors";
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       light: "#ffa4a2",
