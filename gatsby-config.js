28c4193863d3acb12a08926103d2c2b93e68cca9
sakakinox <sakakinox@gmail.com>
2021-12-12 13:53:01 +0000

    uninstall gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,13 +70,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    //{
-    //  resolve: `gatsby-plugin-google-analytics`,
-    //  options: {
-    //    trackingId: `${process.env.GOOGLE_ANALYTICS_ID}`,
-    //    head: true,
-    //  },
-    //},
     {
       resolve: "gatsby-plugin-google-gtag",
       options: {
