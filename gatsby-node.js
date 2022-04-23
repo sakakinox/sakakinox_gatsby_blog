08587b0b6047bd6f391589e639034ae954d8c117
sakakinox <sakakinox@gmail.com>
2022-04-23 11:53:25 +0000

    gen tag pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
@@ -6,7 +7,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-
+  const TagTemplate = path.resolve(`./src/templates/tags.js`)
   // Get all markdown blog posts sorted by date
   const result = await graphql(
     `
@@ -21,10 +22,16 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
               title
               description
               published
+              tags
             }
             fields {
               slug
             }
+          }
+        }
+        tagsGroup: allMarkdownRemark(limit: 2000){
+          group(field: frontmatter___tags){
+            fieldValue
           }
         }
       }
@@ -60,8 +67,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             previousPostId,
             nextPostId,
           },
-        })
-        
+        })    
+    })
+    const tags = result.data.tagsGroup.group
+
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+        component: TagTemplate,
+        context: {
+          tag: tag.fieldValue,
+        },
+      })
     })
   }
 }
