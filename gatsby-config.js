3d2a1ebc08637fcd75fbd52fda596d15631b9f39
sakakinox <sakakinox@gmail.com>
2022-05-28 09:43:03 +0000

    fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,7 +90,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/favicon.png`,
-        output: "/rss.xml",
+        output: `/rss.xml`,
       },
     },
     `gatsby-plugin-react-helmet`,
