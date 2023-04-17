bd933d415d8c167dea3f4e98a94bb3844f363f29
sakakinox <sakakinox@gmail.com>
2023-04-17 15:13:52 +0000

    add seo commponent

--- src/pages/app/roulette.js
+++ src/pages/app/roulette.js
@@ -3,12 +3,14 @@ import React from "react";
 import { graphql } from "gatsby";
 import Roulette from "../../components/Roulette";
 import Layout from "../../components/layout";
+import SEO from "../../components/seo";
 
 function RoulettePage({ data }) {
   const { title } = data.site.siteMetadata;
 
   return (
     <Layout location={"/app/roulette"} title={title}>
+      <SEO title="Roulette App"/>
       <Roulette title={title} />
     </Layout>
   );
