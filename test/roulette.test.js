47e0aa9785811b922b61d812e8203a0cecfc5cbc
sakakinox <sakakinox@gmail.com>
2023-04-17 10:43:56 +0000

    change test

--- test/roulette.test.js
+++ test/roulette.test.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import Roulette from "../src/components/Roulette";
-import { graphql, useStaticQuery } from "gatsby";
 import * as Gatsby from "gatsby";
+import "@testing-library/jest-dom";
 
-global.__PATH_PREFIX__ = "";
 const useStaticQuerySpy = jest.spyOn(Gatsby, "useStaticQuery");
 
 // StaticQueryとuseStaticQueryのモック化
 beforeEach(() => {
-  useStaticQuerySpy.mockImplementation(() => ({
+  useStaticQuerySpy.mockReturnValueOnce({
     site: {
       siteMetadata: {
         title: "Test Title",
       },
     },
-  }));
+  });
 });
+
 
 const data = {
   site: {
@@ -72,14 +72,61 @@ test("spins and displays a random item", async () => {
   });
 });
 
-test("shuffle changes the order of items", () => {
+test("shuffle changes the order of items", async () => {
+  render(<Roulette data={data} />);
+  const input = screen.getByLabelText("Input items (separate by line)");
+  fireEvent.change(input, { target: { value: "Item1\nItem2\nItem3\nItem4" } });
+  const shuffleButton = screen.getByText("Shuffle");
+
+  // シャッフルボタンをクリック
+  fireEvent.click(shuffleButton);
+
+  // 元の順序と異なる順序が表示されるまで待機
+  await waitFor(() => {
+    expect(input.value).not.toBe("Item1\nItem2\nItem3\nItem4");
+  });
+});
+
+// ...
+
+// スピンを押すとスピンが開始されること
+test("spin button starts the spin", async () => {
   render(<Roulette data={data} />);
   const input = screen.getByLabelText("Input items (separate by line)");
   fireEvent.change(input, { target: { value: "Item1\nItem2\nItem3" } });
-  const shuffleButton = screen.getByText("Shuffle");
-  fireEvent.click(shuffleButton);
+  const spinButton = screen.getByText("Spin");
+  fireEvent.click(spinButton);
 
-  // 順番が変わっていることを確認
-  expect(input.value).not.toBe("Item1\nItem2\nItem3");
+  await waitFor(() => {
+    expect(screen.getByText(/Item[1-3]/)).toBeInTheDocument();
+  });
 });
 
+// リストに2バイト文字が入ってもエラーが発生しないこと
+test("2-byte characters in the list do not cause errors", () => {
+  render(<Roulette data={data} />);
+  const input = screen.getByLabelText("Input items (separate by line)");
+  fireEvent.change(input, { target: { value: "こんにちは\nさようなら\nありがとう" } });
+  expect(input.value).toBe("こんにちは\nさようなら\nありがとう");
+});
+
+// それぞれのボタンを押したときにリストの内容がlocalStorageに保存されること
+test("list data is saved to localStorage when buttons are clicked", () => {
+  render(<Roulette />);
+  fireEvent.change(screen.getByLabelText("Input items (separate by line)"), { target: { value: "Item1\nItem2\nItem3" } });
+  const spinButton = screen.getByText("Spin");
+  fireEvent.click(spinButton);
+  expect(localStorage.getItem("rouletteItems")).toBe("Item1,Item2,Item3");
+  
+  const shuffleButton = screen.getByText("Shuffle");
+  fireEvent.click(shuffleButton);
+  expect(localStorage.getItem("rouletteItems")).toBe("Item1,Item2,Item3");
+});
+
+// localStorageにデータがある場合、リストの情報を読み込めること
+test("loads list data from localStorage if available", () => {
+  localStorage.setItem("roulette_list", "Item1\nItem2\nItem3");
+  render(<Roulette data={data} />);
+  const input = screen.getByLabelText("Input items (separate by line)");
+  expect(input.value).toBe("Item1\nItem2\nItem3");
+});
