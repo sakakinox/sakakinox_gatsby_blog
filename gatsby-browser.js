ff04a245660d4db30e710ea8f2d03c10cf5fc213
sakakinox <sakakinox@gmail.com>
2023-05-27 13:50:32 +0000

    post p33

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,15 @@ import "typeface-merriweather"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 import "./styles/style.css"
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react'
+import OgpLink from './src/components/OgpLink'
+
+const components = {
+  OgpLink,
+  // any other components you want as shortcodes
+}
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+)
